032a2274c6fe1e65b813030a4dee51a3427bff13
yangqiuhua <yangqiuhua@ushareit.com>
2024-01-08 18:49:25 +0800

    fix 导入otpauth-migration bug

--- miniprogram/utils/otpauth-migration.ts
+++ miniprogram/utils/otpauth-migration.ts
@@ -2,57 +2,84 @@ import CryptoJS from 'crypto-js';
 import { KeyUriFormat } from './otpauth';
 import { Payload } from './migration';
 
-function wordArrayToUint8Array(wordArray: CryptoJS.lib.WordArray): Uint8Array {
-    const byteArray = new Uint8Array(wordArray.words.length * 4);
-  
-    let offset = 0;
-    for (let i = 0; i < wordArray.words.length; i++) {
-      const word = wordArray.words[i];
-      byteArray[offset++] = (word >> 24) & 0xFF;
-      byteArray[offset++] = (word >> 16) & 0xFF;
-      byteArray[offset++] = (word >> 8) & 0xFF;
-      byteArray[offset++] = word & 0xFF;
+function wordArrayToByteArray(wordArray: CryptoJS.lib.WordArray) {
+  const byteArray: number[] = [];
+  for (let i = 0; i < wordArray.words.length; ++i) {
+    const word = wordArray.words[i];
+    for (let j = 3; j >= 0; --j) {
+      byteArray.push((word >> (8 * j)) & 0xff);
     }
-  
-    return byteArray;
+  }
+  byteArray.length = wordArray.sigBytes;
+  return byteArray;
 }
 
-const base32Encode = (data: Uint8Array) => {
-    const base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-    let base32 = "";
-  
-    let i = 0;
-    let value = 0;
-    let digit = 0;
-    while (i < data.length) {
-      value = (value << 8) | data[i++];
-      digit += 8;
-      while (digit >= 5) {
-        base32 += base32chars[(value >>> (digit - 5)) & 31];
-        digit -= 5;
+function subByteArray(bytes: number[], start: number, length: number) {
+    const subBytes: number[] = [];
+    for (let i = 0; i < length; i++) {
+      subBytes.push(bytes[start + i]);
+    }
+    return subBytes;
+}
+
+function byteArrayToBase32(bytes: number[]) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+    const len = bytes.length;
+    let result = "";
+    let high = 0,
+      low = 0,
+      sh = 0,
+      hasDataInLow = false;
+    for (let i = 0; i < len; i += 5) {
+      hasDataInLow = true;
+      high = 0xf8 & bytes[i];
+      result += chars.charAt(high >> 3);
+      low = 0x07 & bytes[i];
+      sh = 2;
+
+      if (i + 1 < len) {
+        high = 0xc0 & bytes[i + 1];
+        result += chars.charAt((low << 2) + (high >> 6));
+        result += chars.charAt((0x3e & bytes[i + 1]) >> 1);
+        low = bytes[i + 1] & 0x01;
+        sh = 4;
+      }
+
+      if (i + 2 < len) {
+        high = 0xf0 & bytes[i + 2];
+        result += chars.charAt((low << 4) + (high >> 4));
+        low = 0x0f & bytes[i + 2];
+        sh = 1;
+      }
+
+      if (i + 3 < len) {
+        high = 0x80 & bytes[i + 3];
+        result += chars.charAt((low << 1) + (high >> 7));
+        result += chars.charAt((0x7c & bytes[i + 3]) >> 2);
+        low = 0x03 & bytes[i + 3];
+        sh = 3;
+      }
+
+      if (i + 4 < len) {
+        hasDataInLow = false;
+        high = 0xe0 & bytes[i + 4];
+        result += chars.charAt((low << 3) + (high >> 5));
+        result += chars.charAt(0x1f & bytes[i + 4]);
+        low = 0;
+        sh = 0;
       }
     }
-  
-    if (digit > 0) {
-      base32 += base32chars[(value << (5 - digit)) & 31];
-    }
-  
-    return base32;
-};
 
-function otpParameterToLine(otpParameter: Payload.OtpParameters): string {
-    const label = otpParameter.name;
-    const issuer = otpParameter.issuer;
-    const secret = base32Encode(otpParameter.secret);
-    const algorithm = ["SHA1", "SHA1", "SHA256", "SHA512", "MD5"][otpParameter.algorithm];
-    const digits = [6, 6, 8][otpParameter.digits];
-    const type = ["totp", "hotp", "totp"][otpParameter.type];
-    let line = `otpauth://${type}/${label}?secret=${secret}&issuer=${issuer}&algorithm=${algorithm}&digits=${digits}`;
-    if (type === "hotp") {
-        const counter = otpParameter.counter ?? 0;
-        line += `&counter=${counter}`;
+    if (hasDataInLow) {
+      result += chars.charAt(low << sh);
     }
-    return line;
+
+    const padlen = 8 - (result.length % 8);
+    return result + (padlen < 8 ? Array(padlen + 1).join("=") : "");
+}
+
+function byteArrayToString(bytes: number[]) {
+  return String.fromCharCode.apply(null, bytes);
 }
 
 function toOTPAuthURIs(migrationUri: string) {
@@ -61,10 +88,45 @@ function toOTPAuthURIs(migrationUri: string) {
   }
   const base64Data = decodeURIComponent(migrationUri.split("data=")[1]);
   const wordArrayData = CryptoJS.enc.Base64.parse(base64Data);
-  const uint8Array = wordArrayToUint8Array(wordArrayData);
-  // Deserialize from binary
-  const payload:Payload = Payload.deserializeBinary(uint8Array);
-  const lines = payload.otp_parameters.map(otpParameterToLine);
+
+  const byteData = wordArrayToByteArray(wordArrayData);
+  const lines: string[] = [];
+  let offset = 0;
+  while (offset < byteData.length) {
+    if (byteData[offset] !== 10) {
+      break;
+    }
+    const lineLength = byteData[offset + 1];
+    const secretStart = offset + 4;
+    const secretLength = byteData[offset + 3];
+    const secretBytes = subByteArray(byteData, secretStart, secretLength);
+    const secret = byteArrayToBase32(secretBytes);
+    const accountStart = secretStart + secretLength + 2;
+    const accountLength = byteData[secretStart + secretLength + 1];
+    const accountBytes = subByteArray(byteData, accountStart, accountLength);
+    const account = byteArrayToString(accountBytes);
+    const isserStart = accountStart + accountLength + 2;
+    const isserLength = byteData[accountStart + accountLength + 1];
+    const issuerBytes = subByteArray(byteData, isserStart, isserLength);
+    const issuer = byteArrayToString(issuerBytes);
+    const algorithm = ["SHA1", "SHA1", "SHA256", "SHA512", "MD5"][
+      byteData[isserStart + isserLength + 1]
+    ];
+    const digits = [6, 6, 8][byteData[isserStart + isserLength + 3]];
+    const type = ["totp", "hotp", "totp"][
+      byteData[isserStart + isserLength + 5]
+    ];
+    let line = `otpauth://${type}/${account}?secret=${secret}&issuer=${issuer}&algorithm=${algorithm}&digits=${digits}`;
+    if (type === "hotp") {
+      let counter = 0;
+      if (isserStart + isserLength + 7 <= lineLength) {
+        counter = byteData[isserStart + isserLength + 7];
+      }
+      line += `&counter=${counter}`;
+    }
+    lines.push(line);
+    offset += lineLength + 2;
+  }
   return lines;
 }
 
